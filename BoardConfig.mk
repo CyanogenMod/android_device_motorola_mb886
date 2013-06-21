c80f9cef1edbaa7d31a68e6b0dd3f407dd9e0329
dhacker29 <davidhackerdvm@gmail.com>
2013-06-21 00:25:21 -0500

    Enable RTC on all builds

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,9 +31,6 @@ LOCAL_PATH := device/motorola/mb886
 # Use common resources
 TARGET_USES_MOTOROLA_MSM8960_COMMON_LIBLIGHT := true
 
-# Inline kernel building
-TARGET_KERNEL_VARIANT_CONFIG := msm8960_mmi_gsm_defconfig
-
 #bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
